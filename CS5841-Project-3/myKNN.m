1ab756a2ef57945ec85bc80855fb70431908080e
Taylor Morris <tbmorris@mtu.edu>
2016-11-16 11:30:38 -0500

    Add myKNN.m
    The file for the myKNN function. Part 1 of Project 3.

---
+++ CS5841-Project-3/myKNN.m
@@ -0,0 +1,9 @@
+function [ prediction, bestk, errors ] = myKNN( X, Y, Xtest, k)
+%myKNN uses cross validation approach to determine best value for k
+%   Detailed explanation goes here
+
+
+    
+
+end
+
