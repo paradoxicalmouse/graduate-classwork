eec646cb03bdf82a495334e37d8f16490b32b7ee
Taylor Morris <tbmorris@mtu.edu>
2016-12-15 00:55:51 -0500

    Partially through derivation for CS5841 Project 4

--- CS5841 Projects 1 and 2 - MATLAB/homework3.m
+++
@@ -1,8 +0,0 @@
-lambda = [0.01, 0.1, 1, 10, 100, 1000];
-b = ridge(y_train,x_train,  lambda, 0);
-x = [ones([200 1]) x_test];
-y = x*w ;
-yt = repmat(y_test, [1 6]);
-error = sum((y-yt).^2);
-bar(error);
-set(gca, 'XTick', 1:6, 'XTickLabel', lambda)
--- CS5841 Projects 1 and 2 - MATLAB/regressTest.m
+++
@@ -1,6 +0,0 @@
-A = importdata('airfoil_self_noise.dat');
-[m,n]=size(A);
-
-myregression(A,A(:,1:n-1),1);
-% myregression(B,B(:,1:n),1);
-% myregression(C,C(:,1:p),1);
